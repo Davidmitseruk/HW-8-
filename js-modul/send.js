6500c88eac9799554bd594c0a2d514d44efe3016
Davidmitseruk <davidmitseruk@gmail.com>
2025-03-03 15:51:31 +0100

    lol

--- js-modul/send.js
+++ js-modul/send.js
@@ -1,1 +1,26 @@
-export const saveData = 
+
+export const saveData = (event) => {
+    event.preventDefault();
+
+    dataBase.name = name.value;
+    dataBase.email = email.value;
+    dataBase.comment = comment.value;
+    form.reset();
+
+    console.log(dataBase.name, dataBase.email, dataBase.comment);
+    
+    form.style.display = 'none';
+    thanks.style.display = 'block';
+}
+
+const thanks = document.getElementById('thanks');
+const form = document.getElementById('form');
+const name = document.getElementById('nameInp');
+const email = document.getElementById('emailInp');
+const comment = document.getElementById('commentInp');
+
+const dataBase = {
+    name: '',
+    email: '',
+    comment: '',
+}
--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 import{saveData} from "./js-modul/send";
+
+const form = document.getElementById('form');
+form.addEventListener('submit', saveData);
